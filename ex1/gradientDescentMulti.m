f7d904112906fdffc77615ea71aef6bdd6f42cfc
coredux <redpath000@gmail.com>
2015-12-10 22:11:12 +0800

    Gradient Descent for Mutiple Variables

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,16 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    rightSum = zeros( length(theta) , 1 );
+    for i = 1:m;
+        xToi = X(i,:); %xToi 1 * n then xToi' n * 1
+        % theta n * 1 then theta' 1 * n
+        A = theta' * xToi';
+        hypoToi = sum( A(:) );
+        B = ( hypoToi - y(i) ) * xToi;
+        rightSum = rightSum + B';
+    end
+    rightVal = rightSum * alpha / m;
+    theta = theta - rightVal;
 
     % ============================================================
 
